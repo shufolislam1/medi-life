2924bce47a6529ac6ee141277928685a7077c567
shufol islam <shufolislam1255@gmail.com>
2022-04-18 13:24:23 +0600

    slider added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import './Home.css'
+import { Carousel } from 'react-bootstrap';
 import Services from '../Services/Services';
+import slidedoc1 from '../../../img/slidedoce1.jpg'
+import sliderdoc from '../../../img/sliderdoc.jpg'
+import slidedoc3 from '../../../img/sliderdoc3.jpg'
 
 const Home = () => {
     return (
         <div>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slidedoc1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 h-design"
+                        src={sliderdoc}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={slidedoc3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
             <Services></Services>
         </div>
     );
